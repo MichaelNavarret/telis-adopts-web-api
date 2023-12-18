f7a0f5223757fbfa7faff7b795090b044a6f3de3
Michael Navarrete <michael.07navarrete@gmail.coim>
2023-12-18 13:37:34 -0300

    First Commit

---
+++ src/main/java/com/api/telisadoptproyect/TelisAdoptProyectApplication.java
@@ -0,0 +1,13 @@
+package com.api.telisadoptproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelisAdoptProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelisAdoptProyectApplication.class, args);
+	}
+
+}
