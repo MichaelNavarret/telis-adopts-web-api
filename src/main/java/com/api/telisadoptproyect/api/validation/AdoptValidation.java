85fec163df17b06603cb286e7fa97a01f1c5325b
Michael Navarrete <michael.07navarrete@gmail.coim>
2024-04-04 22:51:32 -0300

    Removed bad validation

--- src/main/java/com/api/telisadoptproyect/api/validation/AdoptValidation.java
+++ src/main/java/com/api/telisadoptproyect/api/validation/AdoptValidation.java
@@ -11,6 +11,5 @@ public class AdoptValidation {
    public void updateAdoptParamsValidation(String adoptId, AdoptUpdateRequest request){
        if (StringUtils.isBlank(adoptId)) throw new BadRequestException("The adoptId cannot be null");
        if (request == null) throw new BadRequestException("The request cannot be null");
-       if (StringUtils.isBlank(request.getSpecieId())) throw new BadRequestException("The specieId cannot be null");
    }
 }
